1112eac039f4bbbda30b549f12d2674f3c70d27f
Fellipemj <159451632+Fellipemj@users.noreply.github.com>
2024-11-02 20:27:13 -0300

    Add files via upload

---
+++ Jogo/TikTakToe/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace TikTakToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._9 = new System.Windows.Forms.TextBox();
+            this._8 = new System.Windows.Forms.TextBox();
+            this._7 = new System.Windows.Forms.TextBox();
+            this._6 = new System.Windows.Forms.TextBox();
+            this._5 = new System.Windows.Forms.TextBox();
+            this._4 = new System.Windows.Forms.TextBox();
+            this._3 = new System.Windows.Forms.TextBox();
+            this._2 = new System.Windows.Forms.TextBox();
+            this._1 = new System.Windows.Forms.TextBox();
+            this.txtl = new System.Windows.Forms.TextBox();
+            this.txtc = new System.Windows.Forms.TextBox();
+            this.linha = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_jogo_da_velha = new System.Windows.Forms.Label();
+            this.lbl_aviso = new System.Windows.Forms.Label();
+            this.btn_jogar = new System.Windows.Forms.Button();
+            this.btn_reset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // _9
+            // 
+            this._9.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._9.Location = new System.Drawing.Point(643, 250);
+            this._9.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this._9.Multiline = true;
+            this._9.Name = "_9";
+            this._9.ReadOnly = true;
+            this._9.Size = new System.Drawing.Size(80, 59);
+            this._9.TabIndex = 16;
+            this._9.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this._9.TextChanged += new System.EventHandler(this._9_TextChanged);
+            // 
+            // _8
+            // 
+            this._8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._8.Location = new System.Drawing.Point(545, 250);
+            this._8.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this._8.Multiline = true;
+            this._8.Name = "_8";
+            this._8.ReadOnly = true;
+            this._8.Size = new System.Drawing.Size(82, 59);
+            this._8.TabIndex = 15;
+            this._8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this._8.TextChanged += new System.EventHandler(this._8_TextChanged);
+            // 
+            // _7
+            // 
+            this._7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._7.Location = new System.Drawing.Point(445, 250);
+            this._7.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this._7.Multiline = true;
+            this._7.Name = "_7";
+            this._7.ReadOnly = true;
+            this._7.Size = new System.Drawing.Size(79, 59);
+            this._7.TabIndex = 14;
+            this._7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this._7.TextChanged += new System.EventHandler(this._7_TextChanged);
+            // 
+            // _6
+            // 
+            this._6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._6.Location = new System.Drawing.Point(643, 173);
+            this._6.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this._6.Multiline = true;
+            this._6.Name = "_6";
+            this._6.ReadOnly = true;
+            this._6.Size = new System.Drawing.Size(80, 64);
+            this._6.TabIndex = 13;
+            this._6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this._6.TextChanged += new System.EventHandler(this._6_TextChanged);
+            // 
+            // _5
+            // 
+            this._5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._5.Location = new System.Drawing.Point(545, 173);
+            this._5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this._5.Multiline = true;
+            this._5.Name = "_5";
+            this._5.ReadOnly = true;
+            this._5.Size = new System.Drawing.Size(82, 64);
+            this._5.TabIndex = 12;
+            this._5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this._5.TextChanged += new System.EventHandler(this._5_TextChanged);
+            // 
+            // _4
+            // 
+            this._4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._4.Location = new System.Drawing.Point(445, 173);
+            this._4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this._4.Multiline = true;
+            this._4.Name = "_4";
+            this._4.ReadOnly = true;
+            this._4.Size = new System.Drawing.Size(79, 64);
+            this._4.TabIndex = 11;
+            this._4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this._4.TextChanged += new System.EventHandler(this._4_TextChanged);
+            // 
+            // _3
+            // 
+            this._3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._3.Location = new System.Drawing.Point(643, 99);
+            this._3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this._3.Multiline = true;
+            this._3.Name = "_3";
+            this._3.ReadOnly = true;
+            this._3.Size = new System.Drawing.Size(80, 64);
+            this._3.TabIndex = 10;
+            this._3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this._3.TextChanged += new System.EventHandler(this._3_TextChanged);
+            // 
+            // _2
+            // 
+            this._2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._2.Location = new System.Drawing.Point(545, 99);
+            this._2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this._2.Multiline = true;
+            this._2.Name = "_2";
+            this._2.ReadOnly = true;
+            this._2.Size = new System.Drawing.Size(82, 64);
+            this._2.TabIndex = 9;
+            this._2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this._2.TextChanged += new System.EventHandler(this._2_TextChanged);
+            // 
+            // _1
+            // 
+            this._1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._1.Location = new System.Drawing.Point(445, 99);
+            this._1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this._1.Multiline = true;
+            this._1.Name = "_1";
+            this._1.ReadOnly = true;
+            this._1.Size = new System.Drawing.Size(79, 64);
+            this._1.TabIndex = 8;
+            this._1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this._1.TextChanged += new System.EventHandler(this._1_TextChanged);
+            // 
+            // txtl
+            // 
+            this.txtl.Location = new System.Drawing.Point(489, 469);
+            this.txtl.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtl.Multiline = true;
+            this.txtl.Name = "txtl";
+            this.txtl.Size = new System.Drawing.Size(90, 42);
+            this.txtl.TabIndex = 1;
+            // 
+            // txtc
+            // 
+            this.txtc.Location = new System.Drawing.Point(489, 540);
+            this.txtc.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtc.Multiline = true;
+            this.txtc.Name = "txtc";
+            this.txtc.Size = new System.Drawing.Size(90, 42);
+            this.txtc.TabIndex = 2;
+            // 
+            // linha
+            // 
+            this.linha.AutoSize = true;
+            this.linha.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linha.Location = new System.Drawing.Point(39, 469);
+            this.linha.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.linha.Name = "linha";
+            this.linha.Size = new System.Drawing.Size(429, 40);
+            this.linha.TabIndex = 3;
+            this.linha.Text = "Digite a linha de seleção :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 540);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(449, 40);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Digite a coluna de seleção:";
+            // 
+            // lbl_jogo_da_velha
+            // 
+            this.lbl_jogo_da_velha.AutoSize = true;
+            this.lbl_jogo_da_velha.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_jogo_da_velha.Location = new System.Drawing.Point(460, 22);
+            this.lbl_jogo_da_velha.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_jogo_da_velha.Name = "lbl_jogo_da_velha";
+            this.lbl_jogo_da_velha.Size = new System.Drawing.Size(247, 40);
+            this.lbl_jogo_da_velha.TabIndex = 5;
+            this.lbl_jogo_da_velha.Text = "Jogo da Velha\r\n";
+            // 
+            // lbl_aviso
+            // 
+            this.lbl_aviso.AutoSize = true;
+            this.lbl_aviso.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_aviso.Location = new System.Drawing.Point(579, 335);
+            this.lbl_aviso.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_aviso.Name = "lbl_aviso";
+            this.lbl_aviso.Size = new System.Drawing.Size(0, 46);
+            this.lbl_aviso.TabIndex = 6;
+            // 
+            // btn_jogar
+            // 
+            this.btn_jogar.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_jogar.Location = new System.Drawing.Point(830, 432);
+            this.btn_jogar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_jogar.Name = "btn_jogar";
+            this.btn_jogar.Size = new System.Drawing.Size(148, 82);
+            this.btn_jogar.TabIndex = 7;
+            this.btn_jogar.Text = "Jogar";
+            this.btn_jogar.UseVisualStyleBackColor = true;
+            this.btn_jogar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_reset
+            // 
+            this.btn_reset.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_reset.Location = new System.Drawing.Point(830, 523);
+            this.btn_reset.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_reset.Name = "btn_reset";
+            this.btn_reset.Size = new System.Drawing.Size(148, 82);
+            this.btn_reset.TabIndex = 8;
+            this.btn_reset.Text = "Resetar";
+            this.btn_reset.UseVisualStyleBackColor = true;
+            this.btn_reset.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this._9);
+            this.Controls.Add(this.btn_reset);
+            this.Controls.Add(this._8);
+            this.Controls.Add(this.btn_jogar);
+            this.Controls.Add(this._7);
+            this.Controls.Add(this.lbl_aviso);
+            this.Controls.Add(this._6);
+            this.Controls.Add(this.lbl_jogo_da_velha);
+            this.Controls.Add(this._5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this._4);
+            this.Controls.Add(this.linha);
+            this.Controls.Add(this._3);
+            this.Controls.Add(this.txtc);
+            this.Controls.Add(this._2);
+            this.Controls.Add(this.txtl);
+            this.Controls.Add(this._1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtl;
+        private System.Windows.Forms.TextBox txtc;
+        private System.Windows.Forms.Label linha;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_jogo_da_velha;
+        private System.Windows.Forms.Label lbl_aviso;
+        private System.Windows.Forms.Button btn_jogar;
+        private System.Windows.Forms.TextBox _9;
+        private System.Windows.Forms.TextBox _8;
+        private System.Windows.Forms.TextBox _7;
+        private System.Windows.Forms.TextBox _6;
+        private System.Windows.Forms.TextBox _5;
+        private System.Windows.Forms.TextBox _4;
+        private System.Windows.Forms.TextBox _3;
+        private System.Windows.Forms.TextBox _2;
+        private System.Windows.Forms.TextBox _1;
+        private System.Windows.Forms.Button btn_reset;
+    }
+}
+
---
+++ Jogo/TikTakToe/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TikTakToe
+{
+    public partial class Form1 : Form
+    {
+        string[,] tabuleiro = new string[3, 3];
+        int jogada = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ReiniciarJogo();
+        }
+
+        
+
+        
+
+        
+
+        
+
+        private void _1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (jogada == 0)
+            {
+                ReiniciarJogo();
+            }
+
+            if (!string.IsNullOrEmpty(txtl.Text) && !string.IsNullOrEmpty(txtc.Text))
+            {
+                int linha = int.Parse(txtl.Text);
+                int coluna = int.Parse(txtc.Text);
+
+                if (ValidaJogada(linha, coluna))
+                {
+                    tabuleiro[linha, coluna] = (jogada % 2 == 0) ? "X" : "O";
+                    jogada++;
+                    lbl_aviso.Text = (jogada % 2 == 0) ? "Vez de X jogar" : "Vez de O jogar";
+
+                    if (jogada >= 5 && VerificaVitoria())
+                    {
+                        lbl_aviso.Text = $"Jogador {(jogada % 2 == 1 ? "X" : "O")} venceu!";
+                        jogada = -1;
+                    }
+                }
+                else
+                {
+                    lbl_aviso.Text = "Jogada inválida! Tente novamente.";
+                }
+            }
+            else
+            {
+                lbl_aviso.Text = "Insira valores válidos para linha e coluna!";
+            }
+
+            MostrarTabuleiro();
+            txtl.Clear();
+            txtc.Clear();
+        }
+
+        private void _2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void _3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private bool ValidaJogada(int linha, int coluna)
+        {
+            return linha >= 0 && linha < 3 && coluna >= 0 && coluna < 3 && tabuleiro[linha, coluna] == "";
+        }
+
+        private void ReiniciarJogo()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    tabuleiro[i, j] = "";
+                }
+            }
+            jogada = 0;
+            lbl_aviso.Text = "Vez de X jogar";
+            MostrarTabuleiro();
+        }
+
+        private void _4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MostrarTabuleiro()
+        {
+            _1.Text = tabuleiro[0, 0];
+            _2.Text = tabuleiro[0, 1];
+            _3.Text = tabuleiro[0, 2];
+            _4.Text = tabuleiro[1, 0];
+            _5.Text = tabuleiro[1, 1];
+            _6.Text = tabuleiro[1, 2];
+            _7.Text = tabuleiro[2, 0];
+            _8.Text = tabuleiro[2, 1];
+            _9.Text = tabuleiro[2, 2];
+        }
+
+        private void _5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void _6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void _7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private bool VerificaVitoria()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (tabuleiro[i, 0] != "" && tabuleiro[i, 0] == tabuleiro[i, 1] && tabuleiro[i, 1] == tabuleiro[i, 2])
+                    return true;
+                if (tabuleiro[0, i] != "" && tabuleiro[0, i] == tabuleiro[1, i] && tabuleiro[1, i] == tabuleiro[2, i])
+                    return true;
+            }
+
+            if (tabuleiro[0, 0] != "" && tabuleiro[0, 0] == tabuleiro[1, 1] && tabuleiro[1, 1] == tabuleiro[2, 2])
+                return true;
+
+            if (tabuleiro[0, 2] != "" && tabuleiro[0, 2] == tabuleiro[1, 1] && tabuleiro[1, 1] == tabuleiro[2, 0])
+                return true;
+
+            return false;
+        }
+
+        private void _8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void _9_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
